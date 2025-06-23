d2636d663a701b1d2569f45c22c3863a17bf1f1e
Fwalt237 <patrickkamwa17@gmail.com>
2025-06-23 18:09:40 +0100

    sql

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,2 +1,5 @@
-
-
+create table STUDENT(ID INTEGER not null primary key,NAME varchar,BIRTHDAY DATE,GROUPNUMBER INTEGER);
+create table SUBJECT(ID BIGINT not null primary key,NAME varchar,DESCRIPTION varchar,GRADE INTEGER);
+CREATE TABLE PaymentType(id bigint PRIMARY KEY , name varchar);
+CREATE TABLE Payment(id bigint PRIMARY KEY , type_id bigint, amount decimal, student_id bigint, payment_date datetime, FOREIGN KEY(type_id) REFERENCES PaymentType(id), FOREIGN KEY (student_id) REFERENCES STUDENT(id));
+CREATE TABLE Mark(id bigint PRIMARY KEY, student_id bigint, subject_id bigint, mark int, FOREIGN KEY(student_id) REFERENCES STUDENT(id), FOREIGN KEY(subject_id) REFERENCES SUBJECT(id));
