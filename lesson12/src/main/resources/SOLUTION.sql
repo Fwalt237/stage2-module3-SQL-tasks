73347758bcf445d20ab24404158abb32e83a0ef5
Fwalt237 <patrickkamwa17@gmail.com>
2025-06-24 12:21:42 +0100

    sql2

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-DELETE FROM STUDENT WHERE GROUPNUMBER >= 4;
-DELETE FROM STUDENT WHERE ID IN (SELECT DISTINCT student_id FROM Mark WHERE mark < 4);
-DELETE FROM PaymentType WHERE name = 'DAILY';
-DELETE FROM Mark WHERE mark < 7;
+DELETE FROM mark WHERE student_id IN (SELECT id FROM student WHERE groupnumber >= 4);DELETE FROM payment WHERE student_id IN (SELECT id FROM student WHERE groupnumber >= 4);DELETE FROM student WHERE groupnumber >= 4;
+DELETE FROM mark WHERE student_id IN (SELECT id FROM student WHERE id IN (SELECT DISTINCT student_id FROM mark WHERE mark < 4));DELETE FROM payment WHERE student_id IN (SELECT id FROM student WHERE id IN (SELECT DISTINCT student_id FROM mark WHERE mark < 4));DELETE FROM student WHERE id IN (SELECT DISTINCT student_id FROM mark WHERE mark < 4);
+DELETE FROM payment WHERE type_id = (SELECT id FROM paymenttype WHERE name = 'DAILY'); DELETE FROM paymenttype WHERE name = 'DAILY';
+DELETE FROM mark WHERE mark < 7 AND student_id IN (SELECT id FROM student WHERE groupnumber < 4);
--- lesson2/src/main/resources/SOLUTION.sql
+++ lesson2/src/main/resources/SOLUTION.sql
@@ -1,5 +1,5 @@
-INSERT INTO STUDENT(ID, name,GROUPNUMBER) VALUES(1,'John',1),(2,'Chris', 1),(3,'Carl',1),(4,'Olivier',2),(5,'James',2),(6,'Lucas',2),(7,'Henry',2),(8,'Jacob',3),(9,'Logan',3),(10,'Dylan',4),(11,'Alex',5);
-INSERT INTO SUBJECT(ID, NAME, GRADE) VALUES (1,'Art',1),(2,'Music',1),(3,'Geography',2),(4,'History',2),(5,'PE',3),(6,'Math',3),(7,'Science',4),(8,'IT',4),(9,'Physics',5),(10,'Chemestry',5);
+INSERT INTO STUDENT(ID, name,BIRTHDAY,GROUPNUMBER) VALUES(1,'John','1983-12-12',1),(2,'Chris','1988-10-20', 1),(3,'Carl','1990-11-14',1),(4,'Olivier','1988-02-24',2),(5,'James','1994-11-03',2),(6,'Lucas','1998-10-04',2),(7,'Henry','1992-12-17',2),(8,'Jacob','1955-09-23',3),(9,'Logan','1998-09-07',3),(10,'Dylan','1994-10-25',4),(11,'Alex','2002-08-22',5);
+INSERT INTO SUBJECT(ID, NAME, GRADE) VALUES (1,'Art',1),(2,'Music',1),(3,'Geography',2),(4,'History',2),(5,'PE',3),(6,'Math',3),(7,'Science',4),(8,'IT',4),(9,'Physics',5),(10,'Chemistry',5);
 INSERT INTO PAYMENTTYPE(ID, NAME) VALUES(1,'DAILY'),(2,'WEEKLY'),(3,'MONTHLY');
 INSERT INTO PAYMENT(ID, TYPE_ID, STUDENT_ID) VALUES (1,2,1),(2,3,4),(3,2,7),(4,1,5);
 INSERT INTO MARK VALUES (1,2,1,8),(2,4,4,5),(3,5,3,9),(4,8,6,4),(5,9,5,9),(6,2,9,7),(7,3,10,3);
